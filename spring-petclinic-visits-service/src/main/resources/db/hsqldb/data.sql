4c7fa72e40f37afc0f01067c0a05abfe75ce7056
bogdan.grigorian <bogdan.grigorian@etnetera.cz>
2020-06-07 23:35:59 +0200

    hsqldb - mysql compatibility mode

--- spring-petclinic-visits-service/src/main/resources/db/hsqldb/data.sql
+++ spring-petclinic-visits-service/src/main/resources/db/hsqldb/data.sql
@@ -1,3 +1,5 @@
+SET DATABASE SQL SYNTAX MYS TRUE;
+
 INSERT IGNORE INTO visits VALUES (1, 7, '2013-01-01', 'rabies shot');
 INSERT IGNORE INTO visits VALUES (2, 8, '2013-01-02', 'rabies shot');
 INSERT IGNORE INTO visits VALUES (3, 8, '2013-01-03', 'neutered');
